1f67e97c8449db3f2dfc4dd5a30e95f680cb9513
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-12-16 23:06:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ API_ID = int(os.environ.get("API_ID", "24740695"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "a95990848f2b93b8131a4a7491d97092")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7367343265:AAG8Hs-xOx5GlIooPAryLIDJO5TTKPVsjUc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7718687925:AAHaKS11Trfc7nQztuM_uEmWWgzSBopZgBU")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "Testu172bot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "SANKI_AI_COPYRIGHT_BOT")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "6971100005"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7877197608"))
 # ------------------X------------------------------
 # ------------------------------------------------
 LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002100433415"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Teamsanki:Teamsanki@cluster0.jxme6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # ------------------------------------------------
